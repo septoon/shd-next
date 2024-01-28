c32c90ee511c8c3fb914b3bd5dcc75929485a330
Tigran <41443306+septoon@users.noreply.github.com>
2024-01-28 05:13:05 +0300

    without local storage

--- app/GlobalRedux/Features/cart/cartSlice.js
+++ app/GlobalRedux/Features/cart/cartSlice.js
@@ -1,37 +1,22 @@
-'use client'
-
 import { createSlice } from '@reduxjs/toolkit';
-
-const localItems = localStorage.getItem('items') != null ? JSON.parse(localStorage.getItem('items')) : []
-const localCount = localStorage.getItem('totalCount') != null ? JSON.parse(localStorage.getItem('totalCount')) : 0
-const localPrice = localStorage.getItem('totalPrice') != null ? JSON.parse(localStorage.getItem('totalPrice')) : 0
-
-const setItemFunc = (items, totalCount, totalPrice) => {
-  localStorage.setItem('items', JSON.stringify(items))
-  localStorage.setItem('totalCount', JSON.stringify(totalCount))
-  localStorage.setItem('totalPrice', JSON.stringify(totalPrice))
-}
 
 const cartSlice = createSlice({
   name: 'cart',
   initialState: {
-    items: localItems,
-    totalCount: localCount,
-    totalPrice: localPrice,
+    items: [],
+    totalCount: 0,
+    totalPrice: 0,
   },
   reducers: {
     addDishToCart: (state, action) => {
       state.items.push({ ...action.payload, quantity: 1 });
       state.totalCount += 1;
       state.totalPrice += parseInt(action.payload.price);
-
-      setItemFunc(state.items.map(item => item), state.totalCount, state.totalPrice)
     },
     clearDishCart: (state) => {
       state.items = [];
       state.totalCount = 0;
       state.totalPrice = 0;
-      setItemFunc(state.items.map(item => item), state.totalCount, state.totalPrice)
     },
     removeDish: (state, action) => {
       const { dishId } = action.payload;
@@ -42,7 +27,6 @@ const cartSlice = createSlice({
         state.totalPrice -= parseInt(removedItem.price) * removedItem.quantity;
         state.items = state.items.filter((item) => item.id !== dishId);
       }
-      setItemFunc(state.items.map(item => item), state.totalCount, state.totalPrice)
     },
     incrementDish: (state, action) => {
       const { dishId } = action.payload;
@@ -53,7 +37,6 @@ const cartSlice = createSlice({
         state.totalCount += 1;
         state.totalPrice += parseInt(existingItem.price);
       }
-      setItemFunc(state.items.map(item => item), state.totalCount, state.totalPrice)
     },
     decrementDish: (state, action) => {
       const { dishId } = action.payload;
@@ -73,7 +56,6 @@ const cartSlice = createSlice({
     
         state.items = updatedItems;
       }
-      setItemFunc(state.items.map(item => item), state.totalCount, state.totalPrice)
     },
     
   },
